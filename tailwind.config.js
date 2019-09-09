3c99011cb3d36edfa2d073bcd08aa0e4596243f6
Santos Pierre <santospierreemmanuel@gmail.com>
2019-09-09 23:01:21 +0200

    Ending Mobile View login

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2d7cfe",
+        secondary: "#33CCFF",
+        tertiary: "#42D7A9"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
